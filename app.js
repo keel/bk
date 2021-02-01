4097d98cfdca4056f51aa90b269850b42b4ca794
keel <keel.sike@gmail.com>
2021-02-01 10:26:16 +0800

    去除youtube下载

--- app.js
+++ app.js
@@ -4,10 +4,10 @@
 const bili = require('./lib/bili');
 const packJson = require('./package.json');
 console.log('bk ver:', packJson.version);
-const args = process.argv;
-if (args.length > 2 && args[2].toLowerCase().charAt(0) === 'y') {
-  const youtb = require('./lib/youtb');
-  youtb.main();
-} else {
-  bili.main();
-}
+// const args = process.argv;
+// if (args.length > 2 && args[2].toLowerCase().charAt(0) === 'y') {
+//   const youtb = require('./lib/youtb');
+//   youtb.main();
+// } else {
+// }
+bili.main();
